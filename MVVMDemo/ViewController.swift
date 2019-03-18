3bdf52c3f7173eaed46dec829258efb8e9ebc51b
Shantaram K <shantaramk@mipladmin-5.local>
2019-03-18 14:20:35 +0530

    Initial Commit

---
+++ MVVMDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMDemo
+//
+//  Created by Shantaram K on 18/03/19.
+//  Copyright © 2019 Shantaram K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
